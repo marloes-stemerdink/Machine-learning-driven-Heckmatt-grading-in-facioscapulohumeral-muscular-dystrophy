3715fa65d029e852db7667057899fe69c4d8a4cc
Marloes Stemerdink <marloes.stemerdink@radboudumc.nl>
2026-09-17 10:03:27 +0000

    add class defenition to test.py

--- mmsegmentation/tools/test.py
+++ mmsegmentation/tools/test.py
@@ -7,6 +7,9 @@ from mmengine.config import Config, DictAction
 from mmengine.runner import Runner
 
 
+from mmseg.registry import DATASETS
+from mmseg.datasets import BaseSegDataset
+
 # TODO: support fuse_conv_bn, visualization, and format_only
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -81,6 +84,25 @@ def trigger_visualization_hook(cfg, args):
 
 def main():
     args = parse_args()
+
+    # Define class
+    classes = ('background',
+            'muscle')
+
+    paletteFSHD = [
+            (0, 0, 0),       # black
+        (128, 0, 128),   # purple
+        ]  
+            
+    @DATASETS.register_module()
+    class FSHD(BaseSegDataset):
+        METAINFO = dict(classes = classes, palette = paletteFSHD)
+        def __init__(self, **kwargs):
+            super().__init__(img_suffix='.png',
+                            seg_map_suffix='.png',
+                            reduce_zero_label = False,
+                            ignore_index=255,
+                            **kwargs)
 
     # load config
     cfg = Config.fromfile(args.config)
